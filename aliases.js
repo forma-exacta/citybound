9f83daea3d864fa63e3536158b6d8da57b2b4174
Travis CI User <travis@example.org>
2018-06-11 14:44:28 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES['allocators'] = {};
+ALIASES['chunky'] = {};
+ALIASES['compact'] = {};
+ALIASES['compact_macros'] = {};
+ALIASES['descartes'] = {};
+ALIASES['kay'] = {};
+ALIASES['kay_codegen'] = {};
